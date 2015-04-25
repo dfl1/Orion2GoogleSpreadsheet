2999519a1e9a87e1562ac58cd5409da0de918979
Daniel Fernández Lázaro <daniel.fdezl@outlook.com>
2015-04-25 13:56:53 +0200

    more Updates

--- Orion2GoogleSpreadsheet.py
+++ Orion2GoogleSpreadsheet.py
@@ -1,4 +1,4 @@
-__author__ = 'Daniel Local'
+__author__ = 'Daniel Fernandez Lazaro'
 
 import urllib2
 import webapp2
@@ -18,6 +18,9 @@ from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 from oauth2client import tools
 import yaml
+
+
+logs.config_log()
 
 ### CREDENTIALS from the console ###
 
@@ -90,7 +93,7 @@ class DefaultHandler(webapp2.RequestHandler):
 
         return:None
         """
-        client = get_client_credentials('sheet')
+        client = get_client_credentials('sheets')
         spreadsheet_key = get_spreadsheet_key()
         worksheet_id = 'od6'  # default
 
@@ -98,71 +101,88 @@ class DefaultHandler(webapp2.RequestHandler):
         attributes = []
         headers = {}  # Attributes dict
         rows = []  # Data
+        try:
+            logs.logger.info("Extracting entities")
 
-        for entity in entities:
-            # Extract headers for columns in spreadsheet
-            for attrib in entity['attributes']:
-                attributes.append(str(attrib))
+            for entity in entities:
+                # Extract headers for columns in spreadsheet
+                for attrib in entity['attributes']:
+                    attributes.append(str(attrib))
 
-            # Append rows to insert
-            row = {'id': str(entity['entity_name']), 'date': time.strftime('%m/%d/%Y'), 'time': time.strftime('%H:%M:%S')}
-            for key, value in entity['attributes'].iteritems():
-                row[str(key)] = str(value)
-            rows.append(row)
-            print 'Row: ', row
+                # Append rows to insert
+                row = {'id': str(entity['entity_name']), 'date': time.strftime('%m/%d/%Y'), 'time': time.strftime('%H:%M:%S')}
+                for key, value in entity['attributes'].iteritems():
+                    row[str(key)] = "' " + str(value) # ' to avoid formatting errors when inserting data into spreadsheet's columns
+                rows.append(row)
+                logs.logger.info("Row: " + str(row))
 
-        # Format headers
-        attributes = dict.fromkeys(attributes).keys()
-        attributes.insert(0, 'id')
-        attributes.append('date')
-        attributes.append('time')
-        for i, attrib in enumerate (attributes):
-            headers[i+1] = attrib
+            logs.logger.info("Formatting headers")
 
-        # Check headers in file
-        current_headers = check_headers()
+            # Format headers
+            attributes = dict.fromkeys(attributes).keys()
+            attributes.insert(0, 'id')
+            attributes.append('date')
+            attributes.append('time')
+            for i, attrib in enumerate (attributes):
+                headers[i+1] = attrib
 
-        # If No headers in file
-        if not current_headers:
-            for i, header in enumerate(headers.values()):
-                client.UpdateCell(row=1, col=i + 1, inputValue=header, key=spreadsheet_key, wksht_id=worksheet_id)
-        else:
-            # If New Headers != to Existing Headers
-            if headers != current_headers:
-                # Detect missing headers
-                missing_headers = set(headers.values()) - set(current_headers.values())
-                # Columns to move
-                col_to_move = []
-                # Detect column number for each missing header
-                for h in missing_headers:
-                    h = [key for key, value in headers.iteritems() if value == h][0]
-                    col_to_move.append(h)
-                col_to_move.sort()
+            # Check headers in file
+            current_headers = check_headers()
 
-                if len(col_to_move) == 1:
-                    for i in range(col_to_move[0]+1, max(current_headers.keys())+1):
-                        col_to_move.append(i)
-                    # Higher value (last value) first --> next value...
-                    for col in reversed(col_to_move):
-                        move_column(col,col+1)
-                else:
-                    #Compare col_to_move vs current_headers
-                    matching_col = [item for item in current_headers if item in col_to_move]
-                    for col in reversed(matching_col):
-                        move_column(col,col+len(col_to_move))
-
-                # Insert headers in file
+            # If No headers in file
+            if not current_headers:
+                logs.logger.info("Inserting headers")
                 for i, header in enumerate(headers.values()):
-                    client.UpdateCell(row=1, col=i + 1, inputValue=header, key=spreadsheet_key, wksht_id=worksheet_id)
+                    entry = client.UpdateCell(row=1, col=i + 1, inputValue=header, key=spreadsheet_key, wksht_id=worksheet_id)
+                    if not isinstance(entry, gdata.spreadsheet.SpreadsheetsCell):
+                        logs.logger.warn("Header insert failed: '{0}'".format(entry))
+            else:
+                logs.logger.info("Adjusting headers")
+                # If New Headers differ from Existing Headers
+                if headers != current_headers:
+                    # Detect missing headers
+                    missing_headers = set(headers.values()) - set(current_headers.values())
+                    # Columns to move
+                    col_to_move = []
+                    # Detect column number for each missing header
+                    for h in missing_headers:
+                        h = [key for key, value in headers.iteritems() if value == h][0]
+                        col_to_move.append(h)
+                    col_to_move.sort()
 
-        # Insert rows in file
-        for row in rows:
-            try: #Consider batch request
-                client.InsertRow(row, spreadsheet_key, worksheet_id)
-            except Exception as e:
-                print e
-        print '{} Rows Inserted'.format(len(rows))
+                    if len(col_to_move) == 1:
+                        for i in range(col_to_move[0]+1, max(current_headers.keys())+1):
+                            col_to_move.append(i)
+                        # Higher value (last value) first --> next value...
+                        for col in reversed(col_to_move):
+                            move_column(col,col+1)
+                    else:
+                        #Compare col_to_move vs current_headers
+                        matching_col = [item for item in current_headers if item in col_to_move]
+                        for col in reversed(matching_col):
+                            move_column(col,col+len(col_to_move))
 
+                    # Insert headers in file
+                    for i, header in enumerate(headers.values()):
+                        entry = client.UpdateCell(row=1, col=i + 1, inputValue=header, key=spreadsheet_key, wksht_id=worksheet_id)
+                        if not isinstance(entry, gdata.spreadsheet.SpreadsheetsCell):
+                            logs.logger.warn("Header insert failed: '{0}'".format(entry))
+
+            # Insert rows in file
+            logs.logger.info("Inserting rows")
+            for row in rows:
+                try: ###-Consider batch request-###
+                    entry = client.InsertRow(row, spreadsheet_key, worksheet_id)
+                    if not isinstance(entry, gdata.spreadsheet.SpreadsheetsList):
+                        logs.logger.warn("Row insert failed: '{0}'".format(entry))
+
+                except Exception as e:
+                    logs.logger.error("An error occurred: " + str(e))
+
+            #logs.logger.info(str(len(rows)) + " rows inserted")
+
+        except:
+            logs.logger.warn("An error occurred while inserting data")
 
 ### FUNCTIONS ###
 
@@ -227,25 +247,39 @@ def get_client_credentials(client):
 
     return: Authenticated Drive/Spreadsheet client Object
     """
-    storage = Storage("creds.dat")
-    credentials = storage.get()
+    try:
+        logs.logger.info("Creating storage for credentials")
+        storage = Storage("creds.dat")
+        credentials = storage.get()
 
-    if credentials is None or credentials.invalid:
-        flags = tools.argparser.parse_args(args=[])
-        flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, redirect_uri=REDIRECT_URI)
-        credentials = tools.run_flow(flow, storage, flags)
+        if credentials is None or credentials.invalid:
+            logs.logger.info("Obtaining credentials")
+            flags = tools.argparser.parse_args(args=[])
+            flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, redirect_uri=REDIRECT_URI)
+            credentials = tools.run_flow(flow, storage, flags)
 
-    if credentials.access_token_expired:
-        credentials.refresh(httplib2.Http())
+        if credentials.access_token_expired:
+            logs.logger.info("Refreshing credentials")
+            credentials.refresh(httplib2.Http())
 
-    if client == "drive":
-        dr_client = build('drive', 'v2', http=http)
-        return dr_client
+        if client == "drive":
+            try:
+                logs.logger.info("Creating Drive client")
+                dr_client = build('drive', 'v2', http=http)
+                return dr_client
+            except:
+                logs.logger.warn("An error occurred while creating Drive client")
 
-    elif client == "sheets":
-        sp_client = gdata.spreadsheet.service.SpreadsheetsService(
-        additional_headers={'Authorization': 'Bearer %s' % credentials.access_token})
-        return sp_client
+        elif client == "sheets":
+            try:
+                logs.logger.info("Creating Spreadsheets client")
+                sp_client = gdata.spreadsheet.service.SpreadsheetsService(
+                additional_headers={'Authorization': 'Bearer %s' % credentials.access_token})
+                return sp_client
+            except:
+                logs.logger.warn("An error occurred while creating Spreadsheets client")
+    except:
+        logs.logger.warn("An error occurred while obtaining credentials")
 
 # Insert new file
 def insert_file():
@@ -261,6 +295,7 @@ def insert_file():
         'mimeType': 'application/vnd.google-apps.spreadsheet'
     }
     try:
+        logs.logger.info("Inserting new Spreadsheet file")
         file = drive_service.files().insert(body=body).execute()
         with open('spreadsheet_key.yaml', 'w') as f:
             yaml.safe_dump(file['id'], f, default_flow_style=False)
@@ -268,7 +303,7 @@ def insert_file():
         return None
 
     except errors.HttpError, error:
-        print 'An error occured: %s' % error
+        logs.logger.warn("An error occurred: " + str(error))
         return None
 
 # Get Spreadsheet Key from file
@@ -280,10 +315,12 @@ def get_spreadsheet_key():
     """
     if 'spreadsheet_key.yaml':
         try:
+            logs.logger.info("Checking file Spreadsheet Key")
             with open('spreadsheet_key.yaml') as f:
                 spreadsheet_key = yaml.load(f)
             return spreadsheet_key
         except:
+            logs.logger.info("No Key in file... Obtaining key from Drive account")
             # Get spreadsheet_key
             drive_service = get_client_credentials('drive')
             result = []
@@ -297,7 +334,7 @@ def get_spreadsheet_key():
                     return spreadsheet_key
 
             except errors.HttpError, error:
-                print 'An error occurred: %s' % error
+                logs.logger.warn("An error occurred: " + str(error))
 
 # Check Headers
 def check_headers():
@@ -306,18 +343,23 @@ def check_headers():
 
     return: headers dictionary. Key = Column number (starting from 1):Value = Column name
     """
+    logs.logger.info("Checking headers")
     client = get_client_credentials('sheets')
     spreadsheet_key = get_spreadsheet_key()
     worksheet_id = 'od6'  # default
     headers = {}
 
-    query = gdata.spreadsheet.service.CellQuery()
-    query.max_row = '1'
-    cells = client.GetCellsFeed(spreadsheet_key, wksht_id=worksheet_id, query=query)
-    for i, entry in enumerate(cells.entry):
-        headers[i+1] = entry.cell.text
+    try:
+        query = gdata.spreadsheet.service.CellQuery()
+        query.max_row = '1'
+        cells = client.GetCellsFeed(spreadsheet_key, wksht_id=worksheet_id, query=query)
+        for i, entry in enumerate(cells.entry):
+            headers[i+1] = entry.cell.text
 
-    return headers
+        return headers
+
+    except:
+        logs.logger.warn("An error occurred while checking headers")
 
 # Move columns in Spreadsheet
 def move_column(origin, destination):
@@ -332,53 +374,77 @@ def move_column(origin, destination):
 
     return: None
     """
+    logs.logger.info("Adjusting columns")
+
     from collections import OrderedDict
     client = get_client_credentials('sheets')
     spreadsheet_key = get_spreadsheet_key()
     worksheet_id = 'od6'  # default
     col_values = OrderedDict() #Ordered Dict
 
-    # Origin Column
-    query_orig = gdata.spreadsheet.service.CellQuery()
-    query_orig.return_empty = "true"
-    query_orig.min_col = str(origin)
-    query_orig.max_col = str(origin)
-    cells_orig = client.GetCellsFeed(spreadsheet_key, wksht_id=worksheet_id, query=query_orig)
+    try:
 
-    batch_request_orig = gdata.spreadsheet.SpreadsheetsCellsFeed()
+        try:
+            logs.logger.info("Generating origin column")
+            # Origin Column
+            query_orig = gdata.spreadsheet.service.CellQuery()
+            query_orig.return_empty = "true"
+            query_orig.min_col = str(origin)
+            query_orig.max_col = str(origin)
+            cells_orig = client.GetCellsFeed(spreadsheet_key, wksht_id=worksheet_id, query=query_orig)
 
-    for i, entry_orig in enumerate(cells_orig.entry):
-        col_values[i] = entry_orig.cell.text
-        if entry_orig.cell.text != None: # Avoid inserting new rows at the end due to the initial query
-            entry_orig.cell.inputValue = ''
-            batch_request_orig.AddUpdate(cells_orig.entry[i])
+            batch_request_orig = gdata.spreadsheet.SpreadsheetsCellsFeed()
 
-    #handle empty cells to do destination query
-    col_values_clean = {}
-    for i in enumerate(col_values.items()):
-        if i[1][1] != None:
-            col_values_clean[i[0]] = i[1][1]
+            for i, entry_orig in enumerate(cells_orig.entry):
+                col_values[i] = entry_orig.cell.text
+                if entry_orig.cell.text != None: # Avoid inserting new rows at the end due to the initial query
+                    entry_orig.cell.inputValue = ''
+                    batch_request_orig.AddUpdate(cells_orig.entry[i])
 
-    for x in range(0,max(col_values_clean.keys())+1):
-        if x not in col_values_clean.iterkeys():
-            col_values_clean[x] = '-'
+            # Execute origin column request
+            client.ExecuteBatch(batch_request_orig, cells_orig.GetBatchLink().href)
 
-    # Destination Column
-    query_dest = gdata.spreadsheet.service.CellQuery()
-    query_dest.return_empty = "true"
-    query_dest.max_row = str(len(col_values_clean))
-    query_dest.min_col = str(destination)
-    query_dest.max_col = str(destination)
-    cells_dest = client.GetCellsFeed(spreadsheet_key, wksht_id=worksheet_id, query=query_dest)
+        except:
+            logs.logger.warn("An error occurred while generating origin column")
 
-    batch_request_dest = gdata.spreadsheet.SpreadsheetsCellsFeed()
+        #handle empty cells to do destination query
+        col_values_clean = {}
 
-    for i, entry_dest in enumerate(cells_dest.entry):
-        entry_dest.cell.inputValue = col_values[i]
-        batch_request_dest.AddUpdate(cells_dest.entry[col_values.keys().index(i)])
+        try:
+            for i in enumerate(col_values.items()):
+                if i[1][1] != None:
+                    col_values_clean[i[0]] = i[1][1]
 
-    client.ExecuteBatch(batch_request_orig, cells_orig.GetBatchLink().href)
-    client.ExecuteBatch(batch_request_dest, cells_dest.GetBatchLink().href)
+            for x in range(0,max(col_values_clean.keys())+1):
+                if x not in col_values_clean.iterkeys():
+                    col_values_clean[x] = '-'
+        except:
+            logs.logger.warn("An error occurred while handing empty cells in column")
+
+        try:
+            logs.logger.info("Generating destination column")
+            # Destination Column
+            query_dest = gdata.spreadsheet.service.CellQuery()
+            query_dest.return_empty = "true"
+            query_dest.max_row = str(len(col_values_clean))
+            query_dest.min_col = str(destination)
+            query_dest.max_col = str(destination)
+            cells_dest = client.GetCellsFeed(spreadsheet_key, wksht_id=worksheet_id, query=query_dest)
+
+            batch_request_dest = gdata.spreadsheet.SpreadsheetsCellsFeed()
+
+            for i, entry_dest in enumerate(cells_dest.entry):
+                entry_dest.cell.inputValue = col_values[i]
+                batch_request_dest.AddUpdate(cells_dest.entry[col_values.keys().index(i)])
+
+            # Execute destination column request
+            client.ExecuteBatch(batch_request_dest, cells_dest.GetBatchLink().href)
+
+        except:
+            logs.logger.warn("An error occurred while generating destination column")
+
+    except:
+        logs.logger.warn("An error occurred while moving columns")
 
 
 ### MAIN ###
